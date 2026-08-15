1c77ff6b00f6b1b2011840a2021768d6e509f1e3
Jasson <johnsonjasson517@gmail.com>
2026-08-15 02:33:16 -0400

    feat: verify IR obstacle sensor detection

--- main/main.c
+++ main/main.c
@@ -3,29 +3,40 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define TEST_GPIO GPIO_NUM_2
+#define IR_PIN GPIO_NUM_27
 
 void app_main(void)
 {
-    gpio_reset_pin(TEST_GPIO);
-    gpio_set_direction(TEST_GPIO, GPIO_MODE_OUTPUT);
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << IR_PIN),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
 
-    printf("\n");
-    printf("=====================================\n");
+    gpio_config(&io_conf);
+
+    printf("=================================\n");
     printf(" ESP32 Edge Vision Station\n");
-    printf(" Day 2 - GPIO Hardware Bring-Up\n");
-    printf("=====================================\n");
+    printf(" IR Sensor Test\n");
+    printf("=================================\n");
 
     while (1)
     {
-        gpio_set_level(TEST_GPIO, 1);
-        printf("GPIO 2 -> HIGH\n");
+        int ir_state = gpio_get_level(IR_PIN);
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("GPIO27 = %d\n", ir_state);
 
-        gpio_set_level(TEST_GPIO, 0);
-        printf("GPIO 2 -> LOW\n");
+        if (ir_state == 0)
+        {
+            printf("OBJECT DETECTED\n");
+        }
+        else
+        {
+            printf("NO OBJECT\n");
+        }
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
